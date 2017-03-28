8f9a353524a6353f6eabef2d14c187959d4a0d7f
ramzez <ramzez1983@gmail.com>
2017-03-28 13:31:09 +0200

    small workout on angular

--- frontend/app/scripts/controllers/about.js
+++ frontend/app/scripts/controllers/about.js
@@ -8,10 +8,17 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl', function AboutController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
