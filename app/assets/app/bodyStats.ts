5a45b80b6b8a52e62dbc48d34798e06135ad54ae
ramzez <ramzez1983@gmail.com>
2017-04-12 16:37:35 +0200

    start of work with bodystats view model

---
+++ app/assets/app/bodyStats.ts
@@ -0,0 +1,10 @@
+expot class BodyStats {
+  public id: any;
+  public date: Date;
+  public weight: Number;
+  public fatPercent: Number;
+  public waterPercent: Number;
+  public musclePercent: Number;
+  public bonesPercent: Number;
+  public calories: Number;
+}
